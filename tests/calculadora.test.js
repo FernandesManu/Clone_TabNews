9b053e98feb305e13343a5addc60fc747dfbc34e
Manuella-Fernandes <178603951+FernandesManu@users.noreply.github.com>
2026-08-14 14:13:42 +0000

    test de test

---
+++ tests/calculadora.test.js
@@ -0,0 +1,9 @@
+test("nome do teste", () => {
+  console.log("teste 2");
+  expect(1 + 1).toBe(2);
+});
+
+test("testando outra condição do meu sistema", () => {
+  console.log("teste 3");
+  expect(1 + 1).toBe(2);
+});
