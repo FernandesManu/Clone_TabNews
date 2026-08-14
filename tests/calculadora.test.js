382e4bb1d0640791053b4f73cdb5f451043871e0
Manuella-Fernandes <178603951+FernandesManu@users.noreply.github.com>
2026-08-14 14:39:54 +0000

    um test de verdade

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,9 +1,11 @@
-test("nome do teste", () => {
-  console.log("teste 2");
-  expect(1 + 1).toBe(2);
+const calculadora = require("../models/calculadora");
+
+test("somar 2 + 2 é igual a 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
 });
 
-test("testando outra condição do meu sistema", () => {
-  console.log("teste 3");
-  expect(1 + 1).toBe(2);
+test("somar 2 + 2 é igual a 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
 });
